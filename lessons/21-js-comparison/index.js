2d061cb2b33d59afaac8e24efa2cfbbf5da3a8f3
Huswat-prog <huswatolanite@gmail.com>
2019-10-17 18:52:48 -0600

    exercise21

---
+++ lessons/21-js-comparison/index.js
@@ -0,0 +1,5 @@
+alert("Enter two numbers to see if the first is greater than the second")
+const firstNumber=prompt("what is the first number?");
+const secondNumber=prompt ("what is the second number?");
+const accurate=confirm(firstNumber>SecondNumber);
+console.log("were we accurate?",accurate);
